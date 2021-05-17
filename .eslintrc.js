2465e791e67ab920fd8b43daa2b7fe8c200a1b3e
alterman <1205823081@qq.com>
2021-05-17 16:05:55 +0800

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,45 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+  },
+  globals: {
+    AMap: 'readonly',
+    Loca: 'readonly',
+    gs: 'readonly',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    curly: ['error', 'all'],
+    semi: ['error', 'always'],
+    quotes: ['error', 'single', {
+      avoidEscape: true,
+    }],
+    indent: ['error', 2, {
+      SwitchCase: 1,
+    }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'prefer-const': ['error', {
+      destructuring: 'all',
+      ignoreReadBeforeAssign: false,
+    }],
+    'arrow-parens': ['error', 'always'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always',
+    }],
+    'import/extensions': ['error', 'ignorePackages', {
+      vue: 'never',
+      js: 'never',
+    }],
+  },
+};
