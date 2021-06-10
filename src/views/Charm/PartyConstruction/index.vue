ceac2da168776bea458a44660f12d4d6df57b716
Wayne <ylh941121@gmail.com>
2021-06-10 11:12:49 +0800

    初步修改魅力六横-党群建设

---
+++ src/views/Charm/PartyConstruction/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="party-construction">
+    <div class="left-part">
+      <LeftSection1 class="section-item" />
+      <LeftSection2 class="section-item" />
+    </div>
+
+    <div class="right-part"></div>
+  </div>
+</template>
+<script>
+import LeftSection1 from './LeftSection1';
+import LeftSection2 from './LeftSection2';
+export default {
+  components: {
+    LeftSection1,
+    LeftSection2,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.party-construction{
+  .left-part{
+    position: absolute;
+    width: 170rem;
+    top: 26.3rem;
+    left: 16rem;
+  }
+  .right-part{
+    position: absolute;
+    width: 170rem;
+    right: 16rem;
+    top: 26.3rem;
+  }
+}
+</style>
