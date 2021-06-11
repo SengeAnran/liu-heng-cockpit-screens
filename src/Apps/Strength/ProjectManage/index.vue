667ecbc81dba35c91762d150a29e7644004b6b59
weicw <weicw@citydo.com.cn>
2021-06-11 15:03:59 +0800

    实力-项目管理新建

---
+++ src/Apps/Strength/ProjectManage/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="ProjectManage">ProjectManage</div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectManage',
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.ProjectManage{
+  width: 100%;
+  height: 2070px;
+  background-position: center;
+  background: url('./images/project-manage-bg.png') no-repeat 100% 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
