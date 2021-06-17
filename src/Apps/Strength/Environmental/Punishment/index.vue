52cf066c54eb8c6d08fa18e6e485539daf8c4689
yzx29 <yuzx@citydo.com.cn>
2021-06-17 10:13:01 +0800

    环保专题 页面新增

--- src/Apps/Strength/Environmental/Punishment/index.vue
+++ src/Apps/Strength/Environmental/Punishment/index.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="punishment">
     <BaseTitle title="行政处罚" />
+    <div class="bar-box">
+      <BarItem v-for="(item, index) in list" :key="index" :item="item" />
+    </div>
   </div>
 </template>
 
 <script>
+import BarItem from '../components/barItem';
 export default {
   name: 'Punishment',
   components: {
+    BarItem,
+  },
+  data() {
+    return {
+      list: [
+        { name: '下达限改', number: 50 },
+        { name: '行政处罚', number: 40 },
+        { name: '立案查处', number: 30 },
+        { name: '罚款金额', number: 20 },
+      ],
+    };
   },
 };
 </script>
@@ -18,5 +33,8 @@ export default {
   top: 805px;
   width: 835px;
   height: 535px;
+  .bar-box {
+    height: 400px;
+  }
 }
 </style>
