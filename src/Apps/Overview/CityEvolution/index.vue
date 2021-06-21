bf07a27beba328efdd3fb8c157022398924ad350
libai <libo1@citydo.com.cn>
2021-06-21 15:04:30 +0800

    播放

--- src/Apps/Overview/CityEvolution/index.vue
+++ src/Apps/Overview/CityEvolution/index.vue
@@ -3,18 +3,6 @@
     <LeftComponent />
     <RightComponent />
     <Map />
-    <!-- <div class="marker_wrapper">
-      <img src="./images/mark_left.png" alt="">
-      <div class="address">
-        六横
-      </div>
-      <div class="address_title">
-        户籍人口
-      </div>
-      <div class="count">
-        62824 <span>人</span>
-      </div>
-    </div> -->
   </div>
 </template>
 
@@ -42,60 +30,5 @@ export default {
   // background-size: 100% 100%;
   // background-image: url('./images/show-bg.png');
   // background-image: url('./images/empty_bg.png');
-  .marker_wrapper {
-    position: absolute;
-    top: 50%;
-    left: 44.5%;
-    transform: translate(-50%, -50%);
-    height: 240px;
-    width: 890px;
-    img {
-      height: 100%; width: 100%;
-    }
-    .address {
-      position: absolute;
-      left: 70px;
-      top: 30px;
-      height: 50px;
-      width: 140px;
-      font-size: 40px;
-      font-weight: bolder;
-      color: white;
-    }
-    .address_title {
-      position: absolute;
-      left: 70px;
-      top: 85px;
-      height: 50px;
-      width: 140px;
-      // background: pink;
-      font-size: 24px;
-      font-weight: bolder;
-      color: white;
-    }
-    .count {
-      position: absolute;
-      left: 260px;
-      top: 70px;
-      height: 50px;
-      width: 190px;
-      background: pink;
-      font-weight: bolder;
-      color: white;
-      line-height: 50px;
-      font-size: 42px;
-      background-image:-webkit-linear-gradient(bottom,#B3B3B3,#FFFFFF);
-      -webkit-background-clip:text;
-      -webkit-text-fill-color:transparent;
-      span {
-        position: relative;
-        left: -10px;
-        font-size: 20px;
-        background-image:-webkit-linear-gradient(bottom,#B3B3B3,#FFFFFF);
-        -webkit-background-clip:text;
-        -webkit-text-fill-color:transparent;
-      }
-    }
-  }
 }
 </style>
--- src/Apps/Overview/CityEvolution/map/index.vue
+++ src/Apps/Overview/CityEvolution/map/index.vue
@@ -2,6 +2,36 @@
   <div class="map_wrapper" >
     <div class="mask"></div>
     <div class="main-map" ref="map"></div>
+    <div class="swipper">
+      <div class="line" ref="line">
+        <div class="line_task_point"></div>
+      </div>
+      <div class="start"  @click="autoClick"></div>
+      <div class="start_title" @click="autoClick">
+        开始播放
+      </div>
+      <div class="end" @click="next"></div>
+      <div class="end_title" @click="next">
+        往下切换
+      </div>
+      <div class="line_clum">
+        <div class="item">
+          <span>2000</span>
+        </div>
+        <div class="item">
+          <span>2005</span>
+        </div>
+        <div class="item">
+          <span>2010</span>
+        </div>
+        <div class="item">
+          <span>2015</span>
+        </div>
+      </div>
+      <div class="last">
+        <span>2020</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,12 +46,57 @@ export default {
     return {
       map: null,
       mapDom: null,
+      markList: [],
+      marskLine: '',
+      activeIndex: 0,
+      timer: null,
+      leftMarkMessage: [
+        {
+          name: '常驻人口',
+          count: 2345,
+          unit: '人',
+          position: [122.1902540000, 29.797613],
+        },
+        {
+          name: '流动人口',
+          count: 2345,
+          unit: '人',
+          position: [122.2002540000, 29.767613],
+        },
+        {
+          name: '户籍人口',
+          count: 2345,
+          unit: '人',
+          position: [122.2202540000, 29.727613],
+        },
+      ],
+      rightMarkMessage: [
+        {
+          name: '陆地面积',
+          count: 2345,
+          unit: '平方千米',
+          position: [122.0502540000, 29.787613],
+        },
+        {
+          name: 'GDP',
+          count: 2345,
+          unit: '万元',
+          position: [122.0602540000, 29.757613],
+        },
+        {
+          name: '企业数量',
+          count: 2345,
+          unit: '家',
+          position: [122.0682540000, 29.707613],
+        },
+      ],
     };
   },
   mounted() {
+    this.marskLine = this.$refs.line;
+    this.autoClick();
     this.initMap();
-    this.makeLeftMarker();
-    this.makeRightMarker();
+    this.markerList();
   },
   methods: {
     initMap() {
@@ -29,38 +104,65 @@ export default {
       this.map = new AMap.Map(this.mapDom, {
         resizeEnable: true,
         zoom: 13.4,
-        zooms: [3, 16],
-        center: [122.200254, 29.717613],
+        zoomEnable: false,
+        center: [122.200254, 29.707613],
         mapStyle: 'amap://styles/fd920fcbd2be012ec26b3d6f90c39f09',
       });
     },
-    makeLeftMarker() {
-      const name = '户籍人口';
-      const count = 6284;
-      const unit = '人';
+    markerList() {
+      this.leftMarkMessage.forEach((item) => {
+        this.makeLeftMarker(item);
+      });
+      this.rightMarkMessage.forEach((item) => {
+        this.makeRightMarker(item);
+      });
+    },
+    autoClick() {
+      this.activeIndex = 0;
+      this.lineTask(this.activeIndex); // 进度条
+      this.autoPlay();
+    },
+    autoPlay() {
+      clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        this.paly();
+        this.autoPlay();
+      }, 5 * 1000);
+    },
+    paly() {
+      if (this.activeIndex >= 4) {
+        return;
+      }
+      this.activeIndex += 1;
+      this.lineTask(this.activeIndex); // 进度条
+      // 打点
+    },
+    makeLeftMarker(message) {
       const content = `
         <div class='mark_wrapper_left'>
           <img class='mark_img_bg' src="${require('./img/mark_left.png')}" style=''></img>
           <div class='mark_address'>六横</div>
-          <div class='mark_title'>${name}</div>
+          <div class='mark_title'>${message.name}</div>
           <div class='mark_count'>
-            <span>${count}</span>
+            <span>${message.count}</span>
             <span class='unit'>
-              ${unit}
+              ${message.unit}
             </span>
           </div>
         </div>`
       ;
       const marker = new AMap.Marker({
-        position: [122.2002540000, 29.767613],
+        position: message.position,
         content: content,
       });
       marker.setMap(this.map);
+      this.markList.push(marker);
     },
-    makeRightMarker() {
-      const name = '企业数量';
-      const count = 164;
-      const unit = '家';
+    makeRightMarker(message) {
+      const name = message.name;
+      const count = message.count;
+      const unit = message.unit;
+      const position = message.position;
       const content = `
         <div class='mark_wrapper_right'>
           <img class='mark_img_bg' src="${require('./img/mark_right.png')}" style=''></img>
@@ -75,10 +177,25 @@ export default {
         </div>`
       ;
       const marker = new AMap.Marker({
-        position: [122.0602540000, 29.767613],
+        position: position,
         content: content,
       });
       marker.setMap(this.map);
+      this.markList.push(marker);
+    },
+    lineTask(index) {
+      this.length = 4;
+      this.marskLine.style.width = (1035 / 4) * index + 'px';
+    },
+    next() {
+      clearTimeout(this.timer);
+      if (this.activeIndex === 4) {
+        this.activeIndex = 0;
+        this.lineTask(this.activeIndex); // 进度条
+      } else {
+        this.activeIndex += 1;
+        this.lineTask(this.activeIndex); // 进度条
+      }
     },
   },
 };
@@ -108,6 +225,109 @@ export default {
     right: 0;
     width: 100%;
     height: 2070px;
+  }
+  .swipper {
+    width: 1400px;
+    height: 140px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 50%);
+    background: url('./img/map_swrapper.png') no-repeat;
+    background-size: 100% 100%;
+    z-index: 9999999;
+    .line {
+      width: 1035px;
+      height: 4px;
+      position: absolute;
+      top: 39px;
+      left: 183px;
+      background: #77FFFF;
+      .line_task_point {
+        position: absolute;
+        top: -11px;
+        right: -10px;
+        height: 25px;
+        width: 25px;
+        background: url('./img/now.png') no-repeat;
+        background-size: 100% 100%;
+      }
+    }
+    .start {
+      height: 25px;
+      width: 25px;
+      background: url('./img/start.png') no-repeat;
+      background-size: 100% 100%;
+      position: absolute;
+      left: 110px;
+      top: 30%;
+      transform: translate(-50%, -50%);
+      cursor: pointer;
+    }
+    .end {
+      height: 25px;
+      width: 25px;
+      background: url('./img/end.png') no-repeat;
+      background-size: 100% 100%;
+      position: absolute;
+      right: 90px;
+      top: 30%;
+      transform: translate(-50%, -50%);
+      cursor: pointer;
+    }
+    .start_title {
+      height: 25px;
+      background-size: 100% 100%;
+      position: absolute;
+      left: 112px;
+      top: 50%;
+      font-size: 18px;
+      transform: translate(-50%, -50%);
+      color: white;
+      cursor: pointer;
+      z-index: 10000000;
+    }
+    .end_title {
+      height: 25px;
+      background-size: 100% 100%;
+      position: absolute;
+      right: 42px;
+      top: 50%;
+      font-size: 18px;
+      transform: translate(-50%, -50%);
+      color: white;
+      cursor: pointer;
+    }
+    .line_clum {
+      width: 1035px;
+      height: 30px;
+      position: absolute;
+      top: 70px;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      display: flex;
+      .item {
+        flex: 1;
+        text-anchor: left;
+        height: 100%;
+        color: white;
+        span {
+          font-size: 18px;
+          position: relative;
+          color: white;
+          left: -20px;
+        }
+      }
+    }
+    .last {
+      position: absolute;
+      width: 50px;
+      top: 70px;
+      right: 160px;
+      transform: translate(0%, -50%);
+      font-size: 18px;
+      color: white;
+    }
   }
 }
 </style>
