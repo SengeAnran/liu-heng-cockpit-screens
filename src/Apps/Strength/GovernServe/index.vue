3353b9ec413dd51a4382bcb8aca99fb54c39dcc8
yzx29 <yuzx@citydo.com.cn>
2021-06-22 15:47:33 +0800

    政务服务接口对接完成

--- src/Apps/Strength/GovernServe/index.vue
+++ src/Apps/Strength/GovernServe/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="GovernServe">
+  <div class="govern-serve">
     <Left/>
     <Map/>
     <Right/>
@@ -24,11 +24,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.GovernServe{
+.govern-serve{
   width: 100%;
   height: 2070px;
   background-position: center;
-  background: url('./img/bg.png') no-repeat 100% 100%;
+  // background: url('./img/bg.png') no-repeat 100% 100%;
   position: absolute;
   top: 0;
   left: 0;
