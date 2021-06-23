06174b672cdf4a8e71ef203b2d4bdc074ede1fb3
panguanghong <panguanghong@citydo.com.cn>
2021-06-23 17:06:19 +0800

    活力六横-港口经济 页面开发与接口联调 70%

--- src/Apps/Vitality/PortEconomy/index.vue
+++ src/Apps/Vitality/PortEconomy/index.vue
@@ -1,56 +1,21 @@
 <template>
   <div class="Innovation">
-    <div class="left-webview">
-      <!-- 六横镇科技情况 -->
-      <secondary-title name="六横镇科技情况" />
-      <div class="left-container">
-        <!-- 吞吐量 -->
-        <throughout-capacity />
-        <!-- 码头吞吐量分析 -->
-        <pier-analysis />
-        <!-- 运输货物分类占比 -->
-        <sort-percent />
-        <!-- 运输货物城市分布 -->
-        <city-spread />
-      </div>
-    </div>
-    <div class="center-webview"></div>
-    <div class="right-webview">
-      <div class="right-webview_l">
-        <!-- 六横航运贸易总额 -->
-        <trading-total />
-        <!-- 主要商品贸易额占比TOP5 -->
-        <trading-percent />
-      </div>
-      <!-- 主要城市贸易额排名TOP10 -->
-      <div class="right-webview_r">
-        <secondary-title name="主要城市贸易额排名TOP10" />
-        <trading-rank />
-      </div>
-    </div>
+    <Left />
+    <Map />
+    <Right />
   </div>
 </template>
 
 <script>
-import SecondaryTitle from './components/SecondaryTitle';
-import ThroughoutCapacity from './modules/ThroughoutCapacity';
-import PierAnalysis from './modules/PierAnalysis';
-import SortPercent from './modules/SortPercent';
-import CitySpread from './modules/CitySpread';
-import TradingTotal from './modules/TradingTotal';
-import TradingPercent from './modules/TradingPercent';
-import TradingRank from './modules/TradingRank';
+import Left from './Left/index';
+import Map from './Map';
+import Right from './Right/index';
 export default {
   name: 'PortEconomy',
   components: {
-    SecondaryTitle,
-    ThroughoutCapacity,
-    PierAnalysis,
-    SortPercent,
-    CitySpread,
-    TradingTotal,
-    TradingPercent,
-    TradingRank,
+    Left,
+    Map,
+    Right,
   },
 };
 </script>
@@ -62,51 +27,13 @@ export default {
   display: flex;
   justify-content: space-between;
   padding: 263px 160px 0;
-  & > div {
+  .left-webview,
+  .right-webview {
     width: 1650px;
     height: 980px;
     overflow: hidden;
-  }
-  .left-webview {
-    display: flex;
-    flex-direction: column;
-  }
-  .left-container {
-    flex: 1;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin-top: 20px;
-    & > div {
-      width: 800px;
-      height: 446px;
-      display: flex;
-      flex-wrap: wrap;
-      flex-direction: column;
-      &:nth-child(-n + 2) {
-        margin-bottom: 50px;
-        height: 420px;
-      }
-    }
-  }
-  .right-webview {
-    display: flex;
-    justify-content: space-between;
-    & > div {
-      width: 800px;
-      display: flex;
-      flex-direction: column;
-    }
-    .right-webview_l {
-      display: flex;
-      flex-direction: column;
-      & > div {
-        flex: 1;
-        &:first-child {
-          margin-bottom: 40px;
-        }
-      }
-    }
+    position: relative;
+    z-index: 10;
   }
 }
 </style>
