09fe298a5a82257d190f4e21f0951a0a7e7e5223
alterman <1205823081@qq.com>
2021-06-23 13:04:54 +0800

    接入交通部分数据

--- src/Apps/Overview/Traffic/Bar1/index.vue
+++ src/Apps/Overview/Traffic/Bar1/index.vue
@@ -8,7 +8,8 @@ import { BarChart } from 'echarts/charts';
 import { AxisPointerComponent, GridComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import getOptions from './options';
-import { randomInt } from '../util';
+// import { randomInt } from '../util';
+import trafficAPI from '@/api/Overview/Traffic';
 
 echarts.use([
   BarChart,
@@ -21,14 +22,8 @@ export default {
   data() {
     return {
       list: [
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
-        { name: '名称', value: randomInt(0, 100) },
+        // { name: '名称', value: randomInt(0, 100) },
+        // { name: '名称', value: randomInt(0, 100) },
       ],
     };
   },
@@ -38,6 +33,20 @@ export default {
       getOptions(this.list),
     );
     this.chart = chart;
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const data = await trafficAPI.sourceTrend();
+      // console.log(data);
+      const list = data.map((d) => ({
+        name: d.mc,
+        value: d.kylrs,
+      }));
+      this.chart.setOption(
+        getOptions(list),
+      );
+    },
   },
 };
 </script>
--- src/Apps/Overview/Traffic/Line1/index.vue
+++ src/Apps/Overview/Traffic/Line1/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="line-chart-wrapper">
     <div class="line-chart" ref="chartEle" />
+    <div class="indicator">
+      <span class="name">出租车数量</span>
+      <span class="value" style="width: 16rem;">{{ taxiNum }}</span>
+      <span class="name">日均载客数</span>
+      <span class="value" style="width: 6rem;">{{ busload }}</span>
+    </div>
   </div>
 </template>
 <script>
@@ -9,7 +15,9 @@ import { LineChart } from 'echarts/charts';
 import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import getOptions from './options';
-import { randomInt } from '../util';
+// import { randomInt } from '../util';
+import trafficAPI from '@/api/Overview/Traffic';
+
 echarts.use([
   LineChart,
   CanvasRenderer,
@@ -21,25 +29,63 @@ echarts.use([
 export default {
   data() {
     return {
-      source: Array.from({ length: 12 }).map((_, i) => ({
-        name: `${i + 1}月`,
-        value1: randomInt(100, 200),
-        value2: randomInt(100, 200),
-      })),
+      taxiNum: 0,
+      busload: 0,
+      list: [
+        // { name: '1', value: 12 },
+      ],
     };
   },
   mounted() {
     const chart = echarts.init(this.$refs.chartEle);
-    chart.setOption(getOptions(this.source));
+    chart.setOption(getOptions(this.list));
     this.chart = chart;
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const data = await trafficAPI.taxiAnalysis();
+      // console.log(data);
+      const { czcyytj, czcyyxq } = data;
+      this.taxiNum = czcyytj.czcls;
+      this.busload = czcyytj.rjzkl;
+      const list = czcyyxq.map((d) => ({
+        ...d,
+        name: d.rq,
+        value: d.yycls,
+      }));
+      this.chart.setOption(getOptions(list));
+    },
   },
 };
 </script>
 <style lang="scss" scoped>
 .line-chart-wrapper {
+  position: relative;
   .line-chart {
     width: 100%;
     height: 42rem;
+  }
+  .indicator {
+    position: absolute;
+    right: 0;
+    top: 5px;
+    z-index: 2;
+    .name {
+      font-size: 26px;
+      font-family: Source Han Sans CN;
+      font-weight: 400;
+      color: #FFFFFF;
+      opacity: 0.5;
+    }
+    .value {
+      font-size: 36px;
+      font-family: DINPro;
+      font-weight: 500;
+      color: #FFFFFF;
+      padding-left: 2rem;
+      display: inline-block;
+    }
   }
 }
 </style>
