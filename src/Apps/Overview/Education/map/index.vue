8fa21aa10a91d92a43afc566e83913710f2337af
xujx <xujx@citydo.com.cn>
2021-06-25 11:39:30 +0800

    map

--- src/Apps/Overview/Education/map/index.vue
+++ src/Apps/Overview/Education/map/index.vue
@@ -123,7 +123,6 @@ export default {
   right: 0;
   width: 100%;
   height: 2070px;
-  background-size: 100% 100%;
   .mask {
     position: absolute;
     left: 0;
@@ -143,7 +142,7 @@ export default {
     left: 0;
     right: 0;
     width: 100%;
-    height: 2070px;
+    height: 1350px;
     ::v-deep .custom-marker {
       width: 90px;
       height: 90px;
