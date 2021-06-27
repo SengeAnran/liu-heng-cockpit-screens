7005d2c0b4dc94701757b54cb1a1e218d17c2e49
xujiaxu <296605518@qq.com>
2021-06-27 16:44:04 +0800

    dot

--- src/Apps/Overview/PopulationMap/Map/index.vue
+++ src/Apps/Overview/PopulationMap/Map/index.vue
@@ -16,7 +16,7 @@ export default {
   data() {
     return {
       map: null,
-      markerList: [],
+      popup: null,
     };
   },
   mounted() {
@@ -40,10 +40,14 @@ export default {
     initMap() {
       this.map = new AMap.Map(this.$refs.map, {
         resizeEnable: true,
-        zoom: 13,
+        zoom: 13.3,
         zooms: [3, 20],
         center: [122.138836, 29.730147],
         mapStyle: 'amap://styles/fd920fcbd2be012ec26b3d6f90c39f09',
+      });
+      this.popup = new AMap.InfoWindow({
+        isCustom: true,
+        offset: new AMap.Pixel(0, -30),
       });
     },
     addMarker(data) {
@@ -53,10 +57,16 @@ export default {
           content: '<div class="custom-marker">13444</div>',
         });
         marker.setMap(this.map);
+        marker.on('click', this.markerClick(item));
       });
+    },
+    markerClick(item) {
+      this.popup.setContent(this.createText(item));
+      this.popup.open(this.map, [item.lng, item.lat]);
     },
     createText() {
       return `
+        <div class="info"></div>
       `;
     },
   },
@@ -86,6 +96,66 @@ export default {
     right: 0;
     width: 100%;
     height: 1350px;
+    ::v-deep .custom-marker {
+      position: absolute;
+      width: 71px;
+      height: 61px;
+      background: url('./img/circle.png');
+      &:before {
+        display: block;
+        content: '';
+        width: 70px;
+        height: 50px;
+        background: radial-gradient(transparent 0%,transparent 50%, #ffea78 80%);
+        border: 1px solid #ffea78;
+        box-shadow: 0 0 1px #ffea78;
+        border-radius: 50% / 50%;
+        position: absolute;
+        left: 0;
+        top: 5px;
+        animation: circleChange 3s 1s linear infinite;
+      }
+      &:after {
+        display: block;
+        content: '';
+        width: 70px;
+        height: 50px;
+        background: radial-gradient(transparent 0%,transparent 50%, #ffea78 80%);
+        border: 1px solid #ffea78;
+        box-shadow: 0 0 1px #ffea78;
+        border-radius: 50% / 50%;
+        position: absolute;
+        left: 0;
+        top: 5px;
+        animation: circleChange 3s 2s linear infinite;
+      }
+    }
+    ::v-deep .info {
+      font-size: 30px;
+      color: red;
+    }
+  }
+}
+@keyframes circleChange{
+  0% {
+    transform: scale(1);
+    opacity: 0.6;
+  }
+  25% {
+    transform: scale(1.2);
+    opacity: 0.5;
+  }
+  50% {
+    transform: scale(1.4);
+    opacity: 0.35;
+  }
+  75% {
+    transform: scale(1.6);
+    opacity: 0.15;
+  }
+  100% {
+    transform: scale(1.8);
+    opacity: 0.01;
   }
 }
 </style>
