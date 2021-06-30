011c789b256830576acfbd9cb73b98c52ef527f3
DESKTOP-HBBKP3F\citydo <1270985196@qq.com>
2021-06-30 10:10:01 +0800

    舆情大屏修改

--- src/Apps/Strength/PublicVoice/Left1/index.vue
+++ src/Apps/Strength/PublicVoice/Left1/index.vue
@@ -2,7 +2,7 @@
   <div class="left-1">
     <Title>事件态势</Title>
     <Indicator />
-    <Title style="margin-top: 6.5rem">媒体指数排行</Title>
+    <Title style="margin-top: 6.5rem">热点网站来源</Title>
     <Bar />
   </div>
 </template>
@@ -24,5 +24,6 @@ export default {
   top: 26rem;
   left: 16rem;
   width: 80rem;
+  z-index: 999;
 }
 </style>
