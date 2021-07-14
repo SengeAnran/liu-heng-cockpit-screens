cbbe4bfea568e04e0369d7e934776913cf8365b9
chendl <chendl951028@163.com>
2021-07-14 16:28:56 +0800

    数字乡村地图

--- src/views/Charm/DigitCounty/Comps/Map/index.vue
+++ src/views/Charm/DigitCounty/Comps/Map/index.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="map_wrapper" >
+    <div class="mask"></div>
     <div class="main-map" ref="map"></div>
+    <!-- <div class="map-legend">
+      <p class="legend-title">党群建设图例</p>
+      <ul class="legend-list">
+        <li v-for="(item, index) in legendList" :key="index" @click="selectLegend(item)">
+          <div class="main-label-wrap">
+            <span class="select-rect">
+              <span class="selected-inner" v-show="currentLegend === item.value"></span>
+            </span>
+            {{item.label}}
+          </div>
+          <ul v-if="item.children" class="legend-children-list">
+            <li v-for="(item2, index2) in item.children" :key="index2" @click="selectLegend(item2)">
+              <div class="main-label-wrap">
+                <span class="select-rect">
+                  <span class="selected-inner" v-show="currentLegend === item.value"></span>
+                </span>
+                {{item2.label}}
+              </div>
+
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div> -->
   </div>
 </template>
 
 <script>
-import './mark.scss';
 import AMap from 'AMap';
 export default {
   name: 'CityEvolution',
@@ -14,19 +38,27 @@ export default {
   data() {
     return {
       map: null,
+      mapLayer: null,
+      markerLayer: null,
       mapDom: null,
-      currentLegend: 1,
+      currentLegend: 0,
       legendList: [
-        { value: 1, label: '文化礼堂' },
-        { value: 2, label: '党建中心' },
-        { value: 3, label: '党组织' },
+        { value: 1, label: '党组织' },
+        { value: 2, label: '群团阵地' },
+        {
+          value: 3,
+          label: '新时代站所',
+          children: [
+            { value: 4, label: '文化礼堂' },
+            { value: 5, label: '综合文化站' },
+          ],
+        },
       ],
       lastDetailMarker: null,
     };
   },
   mounted() {
     this.initMap();
-    // this.renderMarker();
   },
   methods: {
     selectLegend(item) {
@@ -36,72 +68,34 @@ export default {
       this.mapDom = this.$refs.map;
       this.map = new AMap.Map(this.mapDom, {
         resizeEnable: true,
-        zoom: 14.4,
+        zoom: 14.8,
         zooms: [3, 16],
-        center: [122.164026, 29.709066],
+        center: [122.187672, 29.69669],
         mapStyle: 'amap://styles/fd920fcbd2be012ec26b3d6f90c39f09',
       });
-      this.mapLayer.on('click', this.handleMarkerClick);
-    },
-    renderMarker() {
-      const type = '文化礼堂';
-      const name = '六横党支部';
-      const content = `
-        <div class="marker-content ${type}">
-          <div class="catalog-content">
-            <h3 class="title">${name}</h3>
-          </div>
-        </div>`
-      ;
-      const marker = new AMap.Marker({
-        position: [122.187672, 29.69669],
-        content: content,
-      });
-      marker.setMap(this.map);
-    },
-    handleMarkerClick(ev) {
-      this.clearLastMarkerDetail();
-      const marker = ev.target;
-      const feature = marker.getExtData();
-      // console.log(marker, feature.properties);
-      const content = this.detailTemplate(feature.properties);
-      marker.setzIndex(1000);
-      marker.setContent(content);
-      this.lastDetailMarker = marker;
-      this.lastDetailMarker.type = this.selected;
-    },
-    clearLastMarkerDetail() {
-      if (!this.lastDetailMarker) {
-        return;
-      }
-      const feature = this.lastDetailMarker.getExtData();
-      this.lastDetailMarker.setzIndex(100);
-      const content = this.markerTemplate(feature.properties);
-      this.lastDetailMarker.setContent(content);
-      this.lastDetailMarker = null;
-    },
-    detailTemplate() {
-      return `
-        <div class="marker-detail-content">
-          <div class="detail-content">
-            <p>六横党支部</p>
-          </div>
-        </div>
-        `;
-    },
-    markerTemplate() {
-      return `
-        <div class="marker-detail-content">
-          <div class="detail-content">
-            <p>六横党支部</p>
-          </div>
-        </div>
-        `;
+      // const mapLayer = new AMap.GeoJSON({
+      //   geoJSON: null,
+      //   getMarker(feature, position) {
+      //     console.log('test1121', feature);
+      //     return new AMap.Marker({
+      //       position,
+      //       content: this.markerTemplate(feature.properties),
+      //       offset: new AMap.Pixel(0, 0),
+      //       extData: feature,
+      //     });
+      //   },
+      // });
+      // mapLayer.setMap(this.map);
+      // this.mapLayer = mapLayer;
     },
   },
 };
 </script>
+<style lang="scss">
+@import "./mark.scss";
+</style>
 <style lang="scss" scoped>
+
 .map_wrapper {
   position: absolute;
   left: 0;
@@ -109,24 +103,35 @@ export default {
   width: 100%;
   height: 2070px;
   background-size: 100% 100%;
-
-  .main-map {
+  .mask {
     position: absolute;
     left: 0;
     right: 0;
     width: 100%;
     height: 2070px;
+    z-index: 6;
+    pointer-events: none;
+    background-size: 100% 100%;
+    background: url('./img/mask_bg.png') no-repeat;
+  }
+  .main-map {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 135rem;
   }
   .map-legend{
     position: absolute;
     z-index: 10;
-    top: 95.6rem;
+    top: 90.6rem;
     right: 37%;
     width: 27.4rem;
-    height: 28.2rem;
+    // height: 28.2rem;
+    height: auto;
     background: url('./img/legend-bg.png') no-repeat 0 0;
     box-sizing: border-box;
-    background-size: 100%;
+    background-size: 100% 100%;
     color: #fff;
     padding: 3rem;
     .legend-title{
@@ -140,14 +145,20 @@ export default {
       font-size: 2.6rem;
       line-height: 2.2;
       li{
-        display: flex;
-        align-items: center;
-        cursor: pointer;
+        .main-label-wrap{
+          display: flex;
+          align-items: center;
+          cursor: pointer;
+        }
+      }
+      .legend-children-list{
+        list-style: none;
+        padding-left: 2rem;
       }
       .select-rect{
         display: inline-block;
-        width: 2.8rem;
-        height: 2.8rem;
+        width: 3rem;
+        height: 3rem;
         box-sizing: border-box;
         margin-right: 1.2rem;
         border: .3rem solid #9BFCFD;
