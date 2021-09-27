e46e230e37c8ef6160b1342e2f3c6e99d4e82941
alterman <1205823081@qq.cim>
2021-09-27 13:36:50 +0800

    添加经济页面打点弹窗

---
+++ src/Apps/Vitality/EconomicDev/newMap/MarkerPopup.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="popup">
+    <div class="scroll">
+      <h3>{{ name }}</h3>
+      <div class="desc">
+        {{ desc }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    feature: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return { age: undefined };
+  },
+  computed: {
+    name() {
+      return this.feature?.properties?.name || this.feature?.properties?._parentProperities?.name || '';
+    },
+    desc() {
+      return this.feature?.properties?.desc || this.feature?.properties?._parentProperities?.desc || '';
+    },
+  },
+  // mounted() {
+  //   console.log('mounted111', this.feature);
+  // },
+  // beforeDestroy() {
+  //   console.log('beforeDestroy');
+  // },
+};
+</script>
+<style lang="scss" scoped>
+.popup {
+  white-space: normal;
+  background: rgba(8, 15, 22, 0.8);
+  border: 1px solid rgba(255, 192, 78, 0.4);
+  color: #fff;
+  padding: 3rem 4rem;
+  min-width: 54rem;
+  .scroll {
+    overflow: auto;
+    max-height: 50rem;
+  }
+  h3 {
+    font-size: 3.4rem;
+    color: #ABFDFE;
+    margin: 2rem 0;
+    margin-top: 0rem;
+  }
+  .desc {
+    font-size: 2.5rem;
+    font-weight: 400;
+    color: #FFFFFF;
+    white-space: pre-wrap;
+  }
+}
+</style>
--- src/Apps/Vitality/EconomicDev/newMap/index.vue
+++ src/Apps/Vitality/EconomicDev/newMap/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map_wrapper" >
     <div class="mask"></div>
-    <div class="main-map" ref="map" v-show="!threeDMap"></div>
+    <Map2d v-show="!threeDMap" />
     <div class="main-map" v-show="threeDMap">
       <iframe src="http://60.163.192.206:8000/srit3d/" width="100%" height="100%"></iframe>
     </div>
@@ -15,10 +15,11 @@
 <script>
 // import './mark.scss';
 import { partyConstruct } from '@/api/Charm/PartyConstruction';
-import AMap from 'AMap';
+import Map2d from './Map2d';
 export default {
   name: 'CityEvolution',
   components: {
+    Map2d,
   },
   data() {
     return {
@@ -31,10 +32,10 @@ export default {
       threeDMap: false,
     };
   },
-  mounted() {
-    this.initMap();
-    this.renderMarker();
-  },
+  // mounted() {
+  //   this.initMap();
+  //   this.renderMarker();
+  // },
   methods: {
     changeMap(type) {
       if (type === 3) {
@@ -46,110 +47,107 @@ export default {
     selectLegend(item) {
       this.currentLegend = item.value;
     },
-    initMap() {
-      this.mapDom = this.$refs.map;
-      this.map = new AMap.Map(this.mapDom, {
-        resizeEnable: true,
-        zoom: 14.8,
-        zooms: [3, 16],
-        center: [122.187672, 29.69669],
-        mapStyle: 'amap://styles/fd920fcbd2be012ec26b3d6f90c39f09',
-      });
-      // const mapLayer = new AMap.GeoJSON({
-      //   geoJSON: null,
-      //   getMarker(feature, position) {
-      //     console.log('test1121', feature);
-      //     return new AMap.Marker({
-      //       position,
-      //       content: this.markerTemplate(feature.properties),
-      //       offset: new AMap.Pixel(0, 0),
-      //       extData: feature,
-      //     });
-      //   },
-      // });
-      // mapLayer.setMap(this.map);
-      // this.mapLayer = mapLayer;
-    },
+    // initMap() {
+    //   this.mapDom = this.$refs.map;
+    //   this.map = new AMap.Map(this.mapDom, {
+    //     resizeEnable: true,
+    //     zoom: 14.8,
+    //     zooms: [3, 16],
+    //     center: [122.187672, 29.69669],
+    //     mapStyle: 'amap://styles/fd920fcbd2be012ec26b3d6f90c39f09',
+    //   });
+    //   // const mapLayer = new AMap.GeoJSON({
+    //   //   geoJSON: null,
+    //   //   getMarker(feature, position) {
+    //   //     console.log('test1121', feature);
+    //   //     return new AMap.Marker({
+    //   //       position,
+    //   //       content: this.markerTemplate(feature.properties),
+    //   //       offset: new AMap.Pixel(0, 0),
+    //   //       extData: feature,
+    //   //     });
+    //   //   },
+    //   // });
+    //   // mapLayer.setMap(this.map);
+    //   // this.mapLayer = mapLayer;
+    // },
     async renderMarker() {
       const result = await partyConstruct().request();
       console.log('map data:', result);
-      const type = '文化礼堂';
-      const name = '舟山市六横猪肉品配送有限公司';
-      const content = `
-        <div class="marker-content ${type}">
-          <div class="catalog-content">
-            <h3 class="title">${name}</h3>
-          </div>
-        </div>`
-      ;
-      const marker = new AMap.Marker({
-        position: [122.187672, 29.69669],
-        content: content,
-      });
-      marker.setMap(this.map);
-      marker.on('click', this.handleMarkerClick);
+      // const type = '文化礼堂';
+      // const name = '舟山市六横猪肉品配送有限公司';
+      // const content = `
+      //   <div class="marker-content ${type}">
+      //     <div class="catalog-content">
+      //       <h3 class="title">${name}</h3>
+      //     </div>
+      //   </div>`
+      // ;
+      // const marker = new AMap.Marker({
+      //   position: [122.187672, 29.69669],
+      //   content: content,
+      // });
+      // marker.setMap(this.map);
+      // marker.on('click', this.handleMarkerClick);
     },
-    handleMarkerClick(ev) {
-      // console.log(ev);
-      this.clearLastMarkerDetail();
-      const marker = ev.target;
+    // handleMarkerClick(ev) {
+    //   // console.log(ev);
+    //   this.clearLastMarkerDetail();
+    //   const marker = ev.target;
 
-      const feature = marker.getExtData();
-      // console.log(marker, feature.properties);
-      const content = this.detailTemplate(feature.properties);
-      marker.setzIndex(1000);
-      marker.setContent(content);
-      this.lastDetailMarker = marker;
-      this.lastDetailMarker.type = this.selected;
-    },
-    clearLastMarkerDetail() {
-      if (!this.lastDetailMarker) {
-        return;
-      }
-      const feature = this.lastDetailMarker.getExtData();
-      this.lastDetailMarker.setzIndex(100);
-      const content = this.markerTemplate(feature.properties);
-      this.lastDetailMarker.setContent(content);
-      this.lastDetailMarker = null;
-    },
-    detailTemplate() {
-      return `
-        <div class="marker-detail-content2">
-          <div class="detail-content">
-            <h3 class="title">舟山市六横猪肉品配送有限公司</h3>
-            <div class="people-row">
-              <div class="item">
-                <div class="sub-title">主要业务：</div>
-                <div class="value">猪肉 配送 生鲜 冷链</div>
-              </div>
-              <div class="item">
-                <div class="sub-title">市值：</div>
-                <div class="value">2456 万元</div>
-              </div>
-              <div class="item">
-                <div class="sub-title">地址：</div>
-                <div class="value">浙江省舟山市普陀区六横镇双塘干岩</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        `;
-    },
-    markerTemplate(data) {
-      return `
-        <div class="marker-content">
-          <div class="catalog-content">
-            <h3 class="title">六横党支部</h3>
-          </div>
-        </div>
-        `;
-    },
+    //   const feature = marker.getExtData();
+    //   // console.log(marker, feature.properties);
+    //   const content = this.detailTemplate(feature.properties);
+    //   marker.setzIndex(1000);
+    //   marker.setContent(content);
+    //   this.lastDetailMarker = marker;
+    //   this.lastDetailMarker.type = this.selected;
+    // },
+    // clearLastMarkerDetail() {
+    //   if (!this.lastDetailMarker) {
+    //     return;
+    //   }
+    //   const feature = this.lastDetailMarker.getExtData();
+    //   this.lastDetailMarker.setzIndex(100);
+    //   const content = this.markerTemplate(feature.properties);
+    //   this.lastDetailMarker.setContent(content);
+    //   this.lastDetailMarker = null;
+    // },
+    // detailTemplate() {
+    //   return `
+    //     <div class="marker-detail-content2">
+    //       <div class="detail-content">
+    //         <h3 class="title">舟山市六横猪肉品配送有限公司</h3>
+    //         <div class="people-row">
+    //           <div class="item">
+    //             <div class="sub-title">主要业务：</div>
+    //             <div class="value">猪肉 配送 生鲜 冷链</div>
+    //           </div>
+    //           <div class="item">
+    //             <div class="sub-title">市值：</div>
+    //             <div class="value">2456 万元</div>
+    //           </div>
+    //           <div class="item">
+    //             <div class="sub-title">地址：</div>
+    //             <div class="value">浙江省舟山市普陀区六横镇双塘干岩</div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     `;
+    // },
+    // markerTemplate(data) {
+    //   return `
+    //     <div class="marker-content">
+    //       <div class="catalog-content">
+    //         <h3 class="title">六横党支部</h3>
+    //       </div>
+    //     </div>
+    //     `;
+    // },
   },
 };
 </script>
-<style lang="scss">
-@import "./mark.scss";
-</style>
 <style lang="scss" scoped>
 
 .map_wrapper {
