dbe0ab589055936456ca0d06638d0227c41c55b2
alterman <1205823081@qq.cim>
2021-10-30 22:56:39 +0800

    add session verify

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routeConfig from '@/config/routes';
+import { parse } from '@/utils/qs';
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,22 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+const session = 'lhzhzz';
+
+function initSession() {
+  const querySession = parse().session;
+  if (querySession === session) {
+    sessionStorage.setItem('querySession', session);
+  }
+}
+initSession();
+router.beforeEach((to, from, next) => {
+  if (location.hostname === 'localhost' || location.hostname === '127.0.0.1' || sessionStorage.getItem('querySession') === session) {
+    return next();
+  }
+  location.href = 'http://60.163.192.206:8000/oa/oauth/authorize?client_id=bigscreen&response_type=code&redirect_uri=http://60.163.192.206:8000/h5app_liuheng/views/liuheng/views/index.html';
 });
 
 export default router;
