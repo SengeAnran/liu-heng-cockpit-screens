7174b98faa7def656ac1f5eedfa9182e9ad2840a
张森云 <zhangsy@citydo.com.cn>
2021-11-24 18:07:40 +0800

    数据分析弹窗

--- src/Apps/Overview/DataAnalysis/index.vue
+++ src/Apps/Overview/DataAnalysis/index.vue
@@ -26,14 +26,29 @@ export default {
       getPageUrl: 'http://10.25.17.237:18155/cas/login?service=http%3A%2F%2F10.25.17.237%3A18155%2Fwydataeye%2Fj_security_check&__t=1637734017419&locale=zh_CN',
       getOpenPageUrl: 'http://10.25.17.237:18155/wydataeye/static/datamap/html/autoIndex.html',
       // getOpenPageUrl: 'https://www.runoob.com/jsref/met-win-open.html',
+      features: 'width=1800,height=700,left=50,top=200'
     };
   },
   mounted() {
+    this.countStyle();
+    window.onresize = () => {
+      this.countStyle();
+    };
   },
   methods: {
+    countStyle() {
+      const clientWidth = document.documentElement.clientWidth
+      const clientHeight = document.documentElement.clientHeight
+      // console.log(document.documentElement);
+      // console.log(document.documentElement.clientHeight);
+      this.features = 'width=' + clientWidth +
+        ',height=' + (parseInt(clientHeight * 0.83)) +
+        ',left=' + 0 +
+        ',top=' + (parseInt(clientHeight * 0.23));
+    },
     goPage() {
       // console.log('click');
-      window.open(this.getOpenPageUrl,'_blank','width=800,height=500,left=500,top=300');
+      window.open(this.getOpenPageUrl,'_blank',this.features);
     }
   },
 };
