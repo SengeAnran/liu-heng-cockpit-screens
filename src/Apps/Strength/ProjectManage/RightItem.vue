c14ce1a3918d46495a06044b1f3bcf3ae1328026
张森云 <zhangsy@citydo.com.cn>
2021-12-27 21:12:14 +0800

    完善

--- src/Apps/Strength/ProjectManage/RightItem.vue
+++ src/Apps/Strength/ProjectManage/RightItem.vue
@@ -6,21 +6,21 @@
           <div class="content">
             <div class="title">{{item.name}}</div>
             <div class="flex">
-<!--              <div @mouseenter="mouseEnter2(item.imgList)" @mouseleave="mouseleave2(item.imgList)" >-->
-<!--                <swiper v-if="item.imgList > 1" ref="myImgSwiper" :options="swiperOption2">-->
-<!--                  <swiper-slider v-for="(item2, index2) in item.imgList" :key="index2">-->
-<!--                    <div v-else class="img">-->
-<!--                      <img :src="'data:image/png;base64,'+item.url" />-->
-<!--                    </div>-->
-<!--                  </swiper-slider>-->
-<!--                </swiper>-->
-<!--                <div v-else class="img">-->
-<!--                  <img :src="'data:image/png;base64,'+item.url" />-->
-<!--                </div>-->
-<!--              </div>-->
-              <div class="img">
-                <img :src="'data:image/png;base64,'+item.url" />
+              <div class="left-img" @mouseenter="mouseEnter2(item.pictures)" @mouseleave="mouseleave2(item.pictures)" >
+                <swiper v-if="item.pictures.length > 1" ref="myImgSwiper" :options="swiperOption2">
+                  <swiper-slider v-for="(item2, index2) in item.pictures" :key="index2">
+                    <div  class="img">
+                      <img :src="'data:image/png;base64,'+item2.tp" />
+                    </div>
+                  </swiper-slider>
+                </swiper>
+                <div v-else class="img">
+                  <img :src="'data:image/png;base64,'+item.pictures[0].tp" />
+                </div>
               </div>
+              <!--              <div class="img">-->
+<!--                <img :src="'data:image/png;base64,'+item.url" />-->
+<!--              </div>-->
               <div class="content-name">
                 {{ item.content }}
               </div>
@@ -29,16 +29,25 @@
         </swiper-slider>
       </swiper>
       <div v-else class="content" v-for="(item, index) in dataList" :key="index">
-      <div class="title">{{item.name}}</div>
-      <div class="flex">
-        <div class="img">
-          <img :src="'data:image/png;base64,'+item.url" />
-        </div>
-        <div class="content-name">
-          {{ item.content }}
+        <div class="title">{{item.name}}</div>
+        <div class="flex">
+          <div class="left-img" @mouseenter="mouseEnter2(item.pictures)" @mouseleave="mouseleave2(item.pictures)" >
+            <swiper v-if="item.pictures && item.pictures.length > 1" ref="myImgSwiper" :options="swiperOption2">
+              <swiper-slider v-for="(item2, index2) in item.pictures" :key="index2">
+                <div class="img">
+                  <img :src="'data:image/png;base64,'+item2.tp" alt="111" />
+                </div>
+              </swiper-slider>
+            </swiper>
+            <div v-else class="img">
+              <img v-if="item.pictures && item.pictures.length > 0" :src="'data:image/png;base64,'+item.pictures[0].tp" alt="111" />
+            </div>
+          </div>
+          <div class="content-name">
+            {{ item.content }}
+          </div>
         </div>
       </div>
-    </div>
     </div>
 <!--    <div class="content">-->
 <!--      <div class="title">小郭巨一期</div>-->
@@ -62,6 +71,9 @@
 <!--          棕榈湾地块位于六横岛西北侧，该区块三面环山、一面临海，远离居民区，可用陆域面积876亩，水、电、道路、通讯等基础设施配套完善，并可根据项目需要进行供冷、供热；紧邻宁波-舟山双屿门航道，具有优良的深水岸线，可用深水岸线约460米，岸滩稳定，不冻不淤，可以建造30万吨级码头。目前已完成对该区域的整体搬迁、土地征用等政策处理工作，并完成了海堤及工作船码头建设、部分开山爆破、场地回填等前期工作。重点引进：油气综合储运、船用低硫燃料油生产、清洁能源生产等项目。-->
 <!--        </div>-->
 <!--      </div>-->
+<!--    </div>-->
+<!--    <div class="test-img">-->
+<!--      <img :src="'data:image/png;base64,'+ testImgUrl" alt="">-->
 <!--    </div>-->
   </div>
 </template>
@@ -104,6 +116,7 @@ export default {
         },
         // autoplay: true,
       },
+      // testImgUrl: '',
     };
   },
   mounted() {
@@ -117,9 +130,10 @@ export default {
           this.dataList.push({
             name: item.xmmc,
             content: item.xmjs,
-            url: item.xmtp,
+            pictures: item.pictures,
           });
         });
+        // this.testImgUrl = this.dataList[0].pictures[0].tp;
       });
     },
     mouseEnter(lists) {
@@ -139,7 +153,7 @@ export default {
       }
     },
     mouseEnter2(lists) {
-      if (lists.length < 1) {
+      if (!lists || lists.length < 1) {
         return;
       }
       if (this.$refs.myImgSwiper) {
@@ -147,7 +161,7 @@ export default {
       }
     },
     mouseleave2(lists) {
-      if (lists.length < 1) {
+      if (!lists || lists.length < 1) {
         return;
       }
       if (this.$refs.myImgSwiper) {
@@ -158,6 +172,11 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+.test-img {
+  width: 400px;
+  height: 300px;
+  background: #FFFFFF;
+}
 img {
   width: 100%;
   height: 100%;
@@ -207,12 +226,18 @@ img {
     //box-sizing: border-box !important;
   }
 }
+.left-img {
+  width: 795px;
+  height: 308px;
+  overflow: hidden;
+}
 .img {
   .swiper-container {
     width: 795px;
     height: 308px;
     .swiper-slide {
-      height: 308px !important;
+      //height: 308px !important;
+      width: 795px !important;
       //box-sizing: border-box !important;
     }
   }
